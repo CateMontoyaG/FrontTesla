02c0b8ba1f1a66f47e16d995a11e2c9ec6546ac0
CateMontoyaG <katee-montoyaa@hotmail.com>
2025-03-21 11:15:38 -0500

    initial commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "~/components/navbar";
+
+export default function Home() {
+    return (
+        <>
+            <div className="h-screen items-center justify-center">
+                <Navbar />
+                <h1 className="text-9xl font-bold uppercase">Hola kate</h1>
+
+            </div>
+        </>
+    )
+}
